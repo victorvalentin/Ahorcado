b8c7be8a71ee46415bcfaa74931b9dc078271628
Viktor <victor_Valentin94@hotmail.com>
2015-11-15 22:07:43 -0300

    Create jugador.c

---
+++ jugador.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#define PUERTO "6669"
+#define BACKLOG 5
+
+int main()
+{
+  return 0;
+}
