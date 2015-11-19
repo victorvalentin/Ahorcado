1c00fadaec9c95452d97697ba2813af52ef504af
Victor <victor_valentin94@hotmail.com>
2015-11-19 14:54:48 -0300

    #2 Agrego headers, y paises.txt con nombre de 25 paises

---
+++ headers.h
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<sys/wait.h>
+#include<arpa/inet.h>
+#include<netdb.h>
+#include<math.h>
+#include<stdio_ext.h>
+#include<time.h>
+#include <signal.h>
